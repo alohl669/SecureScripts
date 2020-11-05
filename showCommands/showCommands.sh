1b01df1951484a1d4810caad0a299f0cb8ff91b2
Alo <alohl669@gmail.com>
2020-11-05 19:13:19 +0100

    Now, we mark date on namefile report

--- showCommands/showCommands.sh
+++ showCommands/showCommands.sh
@@ -10,6 +10,7 @@ cronTasks=$(ps -eo command)
 # declaramos el array que usaremos para los filtros de exclusion y de inclusión
 excluidos=(`cat $blackList`)
 incluidos=(`cat $whiteList`)
+nameReport=$(date +%Y%m%d-%H%M-)report.txt
 
 # FUNCIONES
 . functions.sh
@@ -22,17 +23,17 @@ while getopts "bwa:e:rdh" option;
       e) echo $OPTARG >> $blackList ; exit 1;;
       r) rm $blackList; rm $whiteList; echo "Filtros eliminados"; exit 1;;
       d)
-        date > report.txt
-        echo "--------------------------" >> report.txt
-        echo "Lista de filtros Incluidos" >> report.txt
-        echo ${incluidos[@]} >> report.txt
-        echo "" >> report.txt
-        echo "Lista de filtros excluidos" >> report.txt
-        echo ${excluidos[@]} >> report.txt
-        echo "--------------------------" >> report.txt
-        echo "" >> report.txt
+        date > $nameReport
+        echo "--------------------------" >> $nameReport
+        echo "Lista de filtros Incluidos" >> $nameReport
+        echo ${incluidos[@]} >> $nameReport
+        echo "" >> $nameReport
+        echo "Lista de filtros excluidos" >> $nameReport
+        echo ${excluidos[@]} >> $nameReport
+        echo "--------------------------" >> $nameReport
+        echo "" >> $nameReport
         Filters
-        BucleCommands | tee -a report.txt
+        BucleCommands | tee -a $nameReport
         ;;
       h) Help ; exit 1;;
     #   : ) echo "Invalid Option: -$OPTARG requires an argument" 1>&2 ;;
